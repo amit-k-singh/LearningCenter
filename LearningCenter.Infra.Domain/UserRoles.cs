334b8f7b41712d7ea01f616da1c44c9b47c96f1c
Shubham Desai <desaishubham82@gmail.com>
2023-02-20 11:32:04 +0530

    Added Fields In Technology and User Role Entity

--- LearningCenter.Infra.Domain/UserRoles.cs
+++ LearningCenter.Infra.Domain/UserRoles.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LearningCenter.Infra.Domain
+﻿namespace LearningCenter.Infra.Domain
 {
-    internal class UserRoles
+    public class UserRoles
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
